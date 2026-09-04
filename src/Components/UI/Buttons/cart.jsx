193623be87a0f91ef811935811b6d5bab1e2ebde
Menna-Nabil <mennanabil550@gmail.com>
2026-09-04 22:49:42 +0200

    Update addCart-Btn

--- src/Components/UI/Buttons/cart.jsx
+++ src/Components/UI/Buttons/cart.jsx
@@ -1,13 +1,13 @@
 import '../../../CSS/Buttons.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function AddToCart(){
     return(
-        
-            <button className='add-to-cart-btn'>
-                Add To Cart
-            </button>
-       
+        <button className='add-to-cart-btn'>
+            <FontAwesomeIcon icon={faCartShopping} className="cart-icon" />
+            Add To Cart
+        </button>
     );
-
 }
 export default AddToCart;

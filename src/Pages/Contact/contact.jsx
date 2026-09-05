a73b45d968e16bf43183a906d6c143903c254ed0
Habiba-Rabea <habibarabea205@gmail.com>
2026-09-05 19:55:04 +0200

    merge branches

---
+++ src/Pages/Contact/contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Contact Us Page</h1>
+    </div>
+  );
+};
+
+export default Contact;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MainLayout from "../Components/Layout/mainLayout";
 import Home from '../Pages/Home/home.jsx';
 import Cart from '../Pages/Cart/Cart.jsx';
 import Login from '../Pages/Auth/login.jsx';
 import Wishlist from '../Pages/Wishlist/wishlist.jsx';
 import Checkout from '../Pages/checkout/Checkout.jsx';
+import NotFound from '../pages/Notfound/notfound.jsx';
+import Contact from '../Pages/Contact/contact.jsx';
+
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="wishlist" element={<Wishlist />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
-
   );
 };
-export default AppRoutes;
 
+export default AppRoutes;
